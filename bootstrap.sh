1c0a9675246f052831572eb45e6660058a2695e1
Todd Fisher <todd.fisher@gmail.com>
2009-11-28 15:37:11 -0500

    adding basic GNU configuration

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#autoheader
+mkdir -p m4
+aclocal -I m4
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then 
+  glibtoolize --force
+else
+  libtoolize --force
+fi
+autoconf
+#gtkdocize
+automake --force --add-missing
